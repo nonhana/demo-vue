2b2d17e48de3386468cf278b539ce8b0c0bab943
non_hana <1209220829@qq.com>
2025-01-16 12:08:55 +0800

    feat: 完成简单状态机 parser

---
+++ src/compiler/core.js
@@ -0,0 +1,95 @@
+const State = {
+  initial: 1,
+  tagOpen: 2,
+  tagName: 3,
+  text: 4,
+  tagEnd: 5,
+  tagEndName: 6,
+}
+
+function isAlpha(char) {
+  return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+}
+
+// 有限状态机
+export function tokenize(str) {
+  let currentState = State.initial
+  const chars = [] // 缓存字符
+  const tokens = []
+  // 使用 while 循环开启自动机，只要模板字符串没有被消费完，自动机就会一直运行
+  while (str) {
+    const char = str[0]
+    switch (currentState) {
+      case State.initial:
+        if (char === '<') {
+          currentState = State.tagOpen
+          str = str.slice(1)
+        } else if (isAlpha(char)) {
+          currentState = State.text
+          chars.push(char)
+          str = str.slice(1)
+        }
+        break
+      case State.tagOpen:
+        if (isAlpha(char)) {
+          currentState = State.tagName
+          chars.push(char)
+          str = str.slice(1)
+        } else if (char === '/') {
+          currentState = State.tagEnd
+          str.slice(1)
+        }
+        break
+      case State.tagName:
+        if (isAlpha(char)) {
+          chars.push(char)
+          str = str.slice(1)
+        } else if (char === '>') {
+          currentState = State.initial
+          tokens.push({
+            type: 'tag',
+            name: chars.join(''),
+          })
+          chars.length = 0
+          str = str.slice(1)
+        }
+        break
+      case State.text:
+        if (isAlpha(char)) {
+          chars.push(char)
+          str.slice(1)
+        } else if (char === '<') {
+          currentState = State.tagEnd
+          tokens.push({
+            type: 'text',
+            content: chars.join(''),
+          })
+          chars.length = 0
+          str = str.slice(1)
+        }
+        break
+      case State.tagEnd:
+        if (isAlpha(char)) {
+          currentState = State.tagEndName
+          chars.push(char)
+          str = str.slice(1)
+        }
+        break
+      case State.tagEndName:
+        if (isAlpha(char)) {
+          chars.push(char)
+          str = str.slice(1)
+        } else if (char === '>') {
+          currentState = State.initial
+          tokens.push({
+            type: 'tagEnd',
+            name: chars.join(''),
+          })
+          chars.length = 0
+          str = str.slice(1)
+        }
+        break
+    }
+  }
+  return tokens
+}
---
+++ src/compiler/index.js
@@ -0,0 +1,1 @@
+export { tokenize } from './core.js'
